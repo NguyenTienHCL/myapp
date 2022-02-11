547d446c04f1e12b1437249003a87e1ab29b4f21
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2022-02-11 08:24:54 +0530

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -6,8 +6,8 @@ docker build \
   --build-arg maven_version=3.8.4 \
   --build-arg java_version=11 \
   -t javaapp \
-  -t 2681\javaapp \
+  -t 2681/javaapp \
   .
   
-  docker push 2681\javaapp
+  docker push 2681/javaapp
   
