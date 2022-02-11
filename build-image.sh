d933a650b3e7e3b88c2ceff1728c39fc438b313d
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2022-02-11 08:31:12 +0530

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 docker build \
   --build-arg maven_version=3.8.4 \
   --build-arg java_version=11 \
-  -t javaapp \
   -t 2681/javaapp \
   .
   
+  docker login --username=2681
   docker push 2681/javaapp
   
